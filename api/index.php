7513058a817feef3a590d494933c8774c63b6c3b
zafahi <zackyfirhansyah@gmail.com>
2025-12-17 21:10:42 +0700

    Add Dockerfile for PHP backend

--- api/index.php
+++ api/index.php
@@ -4,8 +4,8 @@
  * Nebeng - Ride Sharing Platform
  */
 
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/config/config.php';
+require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/helpers.php';
 
 setCorsHeaders();
